ee9bb020577f75894c7dbc2d4ab797d606293dc5
luiz-avelar <luiz.avelar25@gmail.com>
2020-12-09 19:08:16 -0300

    Requirements 1 to 6

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+// const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = Boolean(process.env.UPSIDEDOWN_MODE);
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +30,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
