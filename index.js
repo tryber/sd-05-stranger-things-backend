2fd4a757a831a07107f9e1885331626310e989b5
Douglas <douglaza@gmail.com>
2020-12-09 19:03:27 -0300

    new try

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.get('/', (req, res) => {
   res.status(200).json('Olá');
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`'Escutando na porta ${process.env.PORT}'`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
