693feb45194a3eea139793d74b5575704379ded1
Anderson Godoy <anderson.a.godoy@gmail.com>
2020-12-30 15:59:03 -0300

    tentativa 3

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-const { PORT } = process.env;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Escutando na Porta ${PORT}`);
