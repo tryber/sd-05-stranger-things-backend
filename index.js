3aafe4f2e6596b5e8ee6280b7d4f9845cfe1225d
Lucascv <lucascv333@gmail.com>
2021-01-05 11:46:02 -0300

    add 6

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-// const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 
-const hereIsTheUpsideDown = true;
+// const hereIsTheUpsideDown = true;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
