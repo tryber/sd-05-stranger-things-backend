ac2a50faa79757b48fe8bed2a1eb76f0b03e1f03
Andrevlc <andrevezi@gmail.com>
2021-01-06 10:12:27 -0300

    Heroku address fix

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
+// PORTA DO PARANAUE
 const PORT = process.env.PORT || 3000;
-
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
 );
