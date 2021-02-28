328fea751997921c234d83464dc80a128638cc16
Alexander Mitchell <alexlm.trybe@gmail.com>
2021-02-28 17:01:32 -0300

    testando deploy2

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-const PORT = process.env.PORT || 3000;
+const Port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Escutando na porta ${PORT}`);
+app.listen(Port, () => {
+  console.log(`Escutando na porta ${Port}`);
 });
