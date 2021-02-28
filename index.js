f2b4c2e9bbe119960d157801e7d6c8718d83e14a
yoshidalucas92 <lucaslimayoshida@gmail.com>
2021-02-28 18:58:09 -0300

    a

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen((process.env.PORT || 3000), () => {
   console.log(`Escutando na porta ${PORT}`);
 });
