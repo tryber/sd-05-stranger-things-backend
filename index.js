7341a72e0dc230509f50a7b07c350ef13b50c681
yoshidalucas92 <lucaslimayoshida@gmail.com>
2021-02-28 19:13:07 -0300

    a

--- index.js
+++ index.js
@@ -8,28 +8,21 @@ const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
 
-const strangerThingsRepository = new StrangerThingsRepository(
-  strangerThingsDataset,
-);
-const strangerThingsService = new StrangerThingsService(
-  strangerThingsRepository,
-);
+const strangerThingsRepository = new StrangerThingsRepository(strangerThingsDataset);
+const strangerThingsService = new StrangerThingsService(strangerThingsRepository);
 
 app.use(cors());
 
 const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
-  const characters = strangerThingsService.search(
-    req.query,
-    hereIsTheUpsideDown,
-  );
+  const characters = strangerThingsService.search(req.query, hereIsTheUpsideDown);
 
   res.status(200).json(characters);
 });
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen((process.env.PORT || 3000), () => {
-  console.log(`Escutando na porta ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Escutando na porta ${process.env.PORT}`);
 });
