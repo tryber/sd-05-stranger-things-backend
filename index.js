c00d92e46f3fa5627c26d1afcbc509a943eef49e
Sidnei Ramos <sidneiramos02@gmail.com>
2021-02-28 21:48:15 -0300

    Testando

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
+const hereIsTheUpsideDown = true;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
