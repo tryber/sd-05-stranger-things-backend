e8bdd5252fde4f0ac908bb7c247a155a200a81f6
saturnian99 <saturnian.dsg@gmail.com>
2021-03-02 23:07:42 -0300

    Initial; Adding env variables

--- index.js
+++ index.js
@@ -16,17 +16,19 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const UPSIDEDOWN_MODE = process.env.UPSIDEDOWN_MODE || false;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    UPSIDEDOWN_MODE,
   );
 
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
